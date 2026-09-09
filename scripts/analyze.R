5e5045b1ee198834f97224f7aa6b34500a3d04ca
Andrew Crawford <andrew.crawford@gwmail.gwu.edu>
2026-09-09 17:56:30 -0400

    Fix data frame column name and patient ID typo

--- scripts/analyze.R
+++ scripts/analyze.R
@@ -1,6 +1,6 @@
 install.packages("dplyr")
 library(dplyr)
-df <- data.frame(patient_id = c("P001", "P002", "Poo3"), ae = c(54,61,47))
+df <- data.frame(patient_id = c("P001", "P002", "P003"), age = c(54,61,47))
 summary(df)
 renv::snapshot()
 renv::deactivate()
